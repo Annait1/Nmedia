36a6ba222b25d447e8646446dc30511d92ed8685
Anna <serfenix111@mail.ru>
2025-07-12 01:54:10 +0800

    добавила логику для кнопок like, share, форматирование чисел

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,38 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Int,
+    val author: String,
+    val published: String,
+    val content: String,
+    var likes: Int = 999,
+    var likeByMe: Boolean = false,
+    var shares: Int = 1_300_000
+)
+
+fun formatCount(count: Int): String {
+    return when (count) {
+        in 0..999 -> count.toString()
+        in 1000..9999 -> {
+            val thousands = count / 1000
+            val hundreds = (count % 1000) / 100
+            "${thousands}.${hundreds}K"
+        }
+
+        in 10_000..999_999 -> {
+            val thousands = count / 1000
+            "${thousands}K"
+        }
+
+        else -> {
+            val millions = count / 1_000_000
+            val hundredThousands = (count % 1_000_000) / 100_000
+            "${millions}.${hundredThousands}M"
+        }
+    }
+
+}
+
+
+
+
